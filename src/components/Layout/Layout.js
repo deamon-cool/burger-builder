a89b6b6af0641a1916a688ed087cf1b2536efd23
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-08 09:16:23 +0200

    added content for layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Aux from '../../high-order-components/Auxiliary';
 
-const layout = (props) => {
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
-        {props.children}
-    </main>
-}
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
