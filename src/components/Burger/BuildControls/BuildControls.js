b7692bfb311c7ab1c630bf758d88be54f3ccb2bc
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-08 15:12:44 +0200

    in builder controls handler was transfered to builder control

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -15,7 +15,8 @@ const buildControls = (props) => (
             return <BuildControl
                 key={control.label}
                 label={control.label}
-                added={() => props.ingredientAdded(control.type)} />
+                added={() => props.ingredientAdded(control.type)}
+                removed={() => props.ingredientRemoved(control.type)} />
         })}
     </div>
 );
