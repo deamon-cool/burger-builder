f6734b3c9dfa91c50133521ee78bb5448b578ce5
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-08 10:29:30 +0200

    corrected for capital letter

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import classes from './Layout.css';
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.content}>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
