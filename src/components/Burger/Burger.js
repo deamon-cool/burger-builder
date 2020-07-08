14ea4cd262d107747bdad6ac36c10370c1b0b729
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-08 12:51:46 +0200

    changed indent for better readability

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,9 +5,10 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     const transformIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />;
-            });
+            return [...Array(props.ingredients[igKey])]
+                .map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey} />;
+                });
         });
 
     return (
