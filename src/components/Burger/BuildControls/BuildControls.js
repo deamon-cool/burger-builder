92c77fccdae82de74dfd430bafcf998eb612ca22
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-08 14:20:38 +0200

    added mocekd data to the component and added returns of arrays of buildControl

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
 
 const buildControls = (props) => (
-    <div>
-
+    <div className={classes.buildControls}>
+        {controls.map(control => {
+            return <BuildControl key={control.label} label={control.label} />
+        })}
     </div>
 );
 
