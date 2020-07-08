8edcd75b8b90f764764ae54f45763e9c80a4793d
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-08 13:06:57 +0200

    modified component. Thanks to that user can see information abaut empty ingredients in burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformIngredients = Object.keys(props.ingredients)
+    let transformIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />;
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformIngredients.length === 0) {
+        transformIngredients = <p>Please start adding ingredients</p>;
+    }
 
     return (
         <div className={classes.Burger}>
