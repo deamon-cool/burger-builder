e596fd43a7604c4ec0671e30db72aa039e75ddfc
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-08 12:46:36 +0200

    added mocked state to BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,20 @@ import Aux from '../../high-order-components/Auxiliary';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {};
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Aux>
