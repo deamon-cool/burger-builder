c8295e24b0171d240113e1f5017db55ef0956cb1
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-10 14:22:04 +0200

    updated Backrop component, added class css and clicked property handler

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,7 +2,9 @@ import React from 'react';
 import classes from './Backdrop.css';
 
 const backDrop = (props) => (
-    props.show ? <div className={classes.Backdrop}></div> : null
+    props.show ? <div
+        className={classes.Backdrop}
+        onClick={props.clicked}></div> : null
 )
 
 export default backDrop;
