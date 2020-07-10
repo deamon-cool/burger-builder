006f72a3854a5630f62f314e91b3dd8820ab8c8d
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-10 13:34:12 +0200

    implemented style

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Backdrop.css';
 
 const backDrop = (props) => (
-    props.show ? <div></div> : null
+    props.show ? <div className={classes.Backdrop}></div> : null
 )
 
 export default backDrop;
