7dda43e60460a5da9f08274b76619b52b8ecb7c6
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-14 18:59:35 +0200

    added component

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import navigationItems from '../NavigationItems';
+
+const navigationItem = (props) => (
+    <li><a href='/'>A Link</a></li>
+);
+
+export default navigationItems;
