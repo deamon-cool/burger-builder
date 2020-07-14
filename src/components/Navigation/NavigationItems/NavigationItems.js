ba417bc390e4f3b2810d8ecb6565da1e11ad3da2
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-14 19:08:10 +0200

    configured navigation item in navigation items

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-
+        <NavigationItem link='/' active>Burger Builder</NavigationItem>
+        <NavigationItem link='/'>Checkout</NavigationItem>
     </ul>
 )
 
