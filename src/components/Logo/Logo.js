141cd59562e03e13e33f0120062bddae7b57da03
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-15 17:48:14 +0200

    added inline style

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import burgerLogo from '../../assests/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt='MyBurger' />
     </div>
 )
