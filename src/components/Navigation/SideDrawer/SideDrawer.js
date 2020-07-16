2a19097e8e42b20d85183f082e2c9b0ec71945b6
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-16 19:54:05 +0200

    added previous created animation functionality that close the side draw after clicking on back drop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,11 +6,16 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../high-order-components/Auxiliary';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
 
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={classes.SideDrawer}>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
