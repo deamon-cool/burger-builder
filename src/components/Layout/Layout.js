e735c1d24f190b7d08488c8e5ab1ebc6de945ca9
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-16 19:27:43 +0200

    changed function into class and added handler backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,29 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends React.Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+
+}
+
+export default Layout;
