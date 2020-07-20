f0a7e332e16bc125d4acf602fa69eb8d7172ce5b
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-20 15:48:57 +0200

    corrected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Lauout from './components/Layout/Layout';
+import Lauout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
