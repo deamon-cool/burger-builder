91da1a0aab4c57521ce86d23066b12276cd380f8
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-29 20:14:53 +0200

    updated class spinner

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Spinner.css';
 
 const spinner = () => (
-    <div class="Loader">Loading...</div>
+    <div class={classes.Loader}>Loading...</div>
 );
 
 export default spinner;
