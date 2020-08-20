926ae59e2fca8b595a9956a2403b8908970624ba
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-20 17:05:09 +0200

    added component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import Lauout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Lauout>
         <BurgerBuilder />
+        <Checkout />
       </Lauout>
     </div>
   );
