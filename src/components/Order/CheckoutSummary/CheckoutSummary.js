25b9582a9cf41b246067f47cae7ed64164a79cd5
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-20 16:42:12 +0200

    added checkout summary component

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>We hope it tastest well !</h1>
+            <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
+
+            </div>
+        </div>
+    );
+}
+
+export default checkoutSummary;
