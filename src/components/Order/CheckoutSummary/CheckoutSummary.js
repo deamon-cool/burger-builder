f8e72e5e53f3b8fb8e959eb8c672093dea0bf254
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-20 16:47:41 +0200

    updated component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
 const checkoutSummary = (props) => {
     return (
         <div>
             <h1>We hope it tastest well !</h1>
             <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
-
+                <Burger ingredients={props.ingredients} />
             </div>
+            <Button btnType='Danger' />
+            <Button btnType='Success' />
         </div>
     );
 }
