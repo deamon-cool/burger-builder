91dd91e894ea9bc95bef65c3648c5c2d6b1a395f
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-20 17:00:37 +0200

    updated component, added mocked ingredients

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-class Checkout extends React.Component {
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
+class Checkout extends React.Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 1,
+            meat: 1,
+            bacon: 1
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Checkout ingredients={this.state.ingredients} />
+            </div>
+        );
+    }
 }
 
 export default Checkout;
