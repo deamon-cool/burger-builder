9e9259d42046351fd5cf436d5e808e8cbfcadf6c
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-20 16:56:16 +0200

    added style module to component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
 
 const checkoutSummary = (props) => {
     return (
-        <div className=''>
+        <div className={classes.CheckoutSummary}>
             <h1>We hope it tastest well !</h1>
             <div style={{ width: '300px', height: '300px', margin: 'auto' }}>
                 <Burger ingredients={props.ingredients} />
