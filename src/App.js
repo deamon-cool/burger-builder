198d9b990b92dcc3b217e742cedd8b28813d4f36
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-24 17:07:31 +0200

    added route in app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Lauout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends React.Component {
 
@@ -12,6 +13,7 @@ class App extends React.Component {
         <div>
           <Lauout>
             <Route path='/' exact component={BurgerBuilder} />
+            <Route path='/checkout' exact component={Checkout} />
           </Lauout>
         </div>
       </BrowserRouter>
