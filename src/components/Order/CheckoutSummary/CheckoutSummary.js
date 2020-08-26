25d95adbf81ce4c4d71f827852b6d8f8dc7ae151
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-26 16:08:51 +0200

    added props that will be handle in component above

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -13,10 +13,10 @@ const checkoutSummary = (props) => {
             </div>
             <Button
                 btnType='Danger'
-                clicked>CANCEL</Button>
+                clicked={props.onCheckoutCancelled}>CANCEL</Button>
             <Button
                 btnType='Success'
-                clicked>CONTINUE</Button>
+                clicked={props.onCheckoutContinued}>CONTINUE</Button>
         </div>
     );
 }
