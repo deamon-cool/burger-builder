3f30980773643996eff08c2beb7c4639559fbf0a
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-26 16:37:53 +0200

    created new form component

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+
+class ContactData extends React.Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Enter your Contact</h1>
+                <form>
+                    <input type='text' name='name' placeholder='Name' />
+                    <input type='email' name='email' placeholder='Email' />
+                    <input type='text' name='street' placeholder='Street' />
+                    <input type='text' name='postal' placeholder='Postal Code' />
+                    <Button btnType='Success'>ORDER</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ContactData;
