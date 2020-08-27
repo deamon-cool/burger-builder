4103c5bce9f441f2f57ca00f12cf2af519380c70
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-27 19:09:27 +0200

    added route in App to Orders

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Lauout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Lauout>
         <Switch>
           <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
           <Route path='/' exact component={BurgerBuilder} />
         </Switch>
       </Lauout>
