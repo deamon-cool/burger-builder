51dfe66e189b0fed731e0c5c8c9104616598fb40
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-27 18:43:36 +0200

    added spinner during sending data to the server

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import config from '../../../config-fetch';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends React.Component {
     state = {
@@ -52,16 +53,23 @@ class ContactData extends React.Component {
     }
 
     render() {
+        let form = (
+            <form>
+                <input className={classes.Input} type='text' name='name' placeholder='Name' />
+                <input className={classes.Input} type='email' name='email' placeholder='Email' />
+                <input className={classes.Input} type='text' name='street' placeholder='Street' />
+                <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
+                <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
+
         return (
             <div className={classes.ContactData}>
                 <h1>Enter your Contact</h1>
-                <form>
-                    <input className={classes.Input} type='text' name='name' placeholder='Name' />
-                    <input className={classes.Input} type='email' name='email' placeholder='Email' />
-                    <input className={classes.Input} type='text' name='street' placeholder='Street' />
-                    <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
-                    <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
