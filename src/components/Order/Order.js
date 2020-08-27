01920ef19c2ded9dbf4de6900adf9bbf15d6f6c1
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-27 19:03:19 +0200

    created one more component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const order = (props) => (
+    <div>
+        <p>Ingredients: Salad (1)</p>
+        <p>Price: <strong>USD 5.45</strong></p>
+    </div>
+);
+
+export default order;
