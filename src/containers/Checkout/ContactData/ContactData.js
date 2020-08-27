d63040b6d877431ce1933f1ced297d993411dfb0
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-27 18:09:11 +0200

    added sending order to the server

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import config from '../../../config-fetch';
 
 class ContactData extends React.Component {
     state = {
@@ -10,11 +11,47 @@ class ContactData extends React.Component {
         address: {
             street: '',
             postalCode: ''
-        }
+        },
+        loading: false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
+
+        this.setState({ loading: true });
+
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Der Damin',
+                address: {
+                    street: 'Street nr 1',
+                    zipCode: '42211',
+                    country: 'Albania'
+                },
+                email: 'test@test.com'
+            },
+            deliverMethod: 'fastest'
+        }
+
+        const init = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(order)
+        };
+
+        fetch(config.url + 'orders.json', init)
+            .then(res => {})
+            .catch(err => {
+                this.setState({ errorPost: err })
+            })
+            .finally(() => {
+                this.setState({
+                    loading: false,
+                    purchasing: false
+                });
+            });
     }
 
     render() {
