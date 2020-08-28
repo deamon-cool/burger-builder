004f1c6feed8df237d3abbc7e670ebdecc33e1ab
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-28 15:21:34 +0200

    added fetching data from the server in Orders component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import config from '../../config-fetch';
+
 import Order from '../../components/Order/Order';
 
 class Orders extends React.Component {
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        const init = {
+            method: 'get',
+            headers: { 'Content-Type': 'application/json' }
+        };
+
+        fetch(config.url + 'orders.json', init)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                this.setState({ loading: false });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            });
+    }
 
     render() {
         return (
