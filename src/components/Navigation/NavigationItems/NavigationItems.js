18888f1169d07cb78e1adf408488a1fe9cd91cd8
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-28 14:42:21 +0200

    added new navigation item

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/' active>Burger Builder</NavigationItem>
-        <NavigationItem link='/'>Checkout</NavigationItem>
+        <NavigationItem link='/orders'>Orders</NavigationItem>
+        {/* <NavigationItem link='/'>Checkout</NavigationItem> */}
     </ul>
 )
 
