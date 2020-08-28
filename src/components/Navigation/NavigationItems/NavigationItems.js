88a05edf21f7a084d34b6766a1c0437db3515dc4
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-28 14:44:52 +0200

    deleted active

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link='/' active>Burger Builder</NavigationItem>
+        <NavigationItem link='/'>Burger Builder</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
         {/* <NavigationItem link='/'>Checkout</NavigationItem> */}
     </ul>
