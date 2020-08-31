92c418dcba8f5e385fd0eaf76924e56e769c5a61
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-31 18:15:15 +0200

    added key to avoid error key missing in list, added also style

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -12,7 +12,15 @@ const order = (props) => {
     }
 
     const ingredientOutput = ingredients.map(ig => {
-        return <span key={ig.name}>{ig.name} ({ig.amount})</span>;
+        return <span
+            key={ig.name}
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}>{ig.name} ({ig.amount})</span>;
     });
 
     return (
