26f585ca7cd9141f52214617037a1b57716ff798
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-01 20:27:10 +0200

    corrected due to error message abaout sensitive capital letter 'T

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -56,10 +56,10 @@ class ContactData extends React.Component {
     render() {
         let form = (
             <form>
-                <Input inputType='input' type='text' name='name' placeholder='Name' />
-                <Input inputType='input' type='email' name='email' placeholder='Email' />
-                <Input inputType='input' type='text' name='street' placeholder='Street' />
-                <Input inputType='input' type='text' name='postal' placeholder='Postal Code' />
+                <Input inputtype='input' type='text' name='name' placeholder='Name' />
+                <Input inputtype='input' type='email' name='email' placeholder='Email' />
+                <Input inputtype='input' type='text' name='street' placeholder='Street' />
+                <Input inputtype='input' type='text' name='postal' placeholder='Postal Code' />
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
