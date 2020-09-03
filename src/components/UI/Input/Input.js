191e126f0870195aa4f0777dac944ed7776b61b5
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-03 21:10:44 +0200

    added new input option, select

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -15,6 +15,21 @@ const input = (props) => {
                 className={classes.InputElement}
                 {...props.elementConfig} value={props.value} />
             break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option
+                            key={option.value}
+                            value={props.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
         default:
             inputElement = <input
                 className={classes.InputElement}
