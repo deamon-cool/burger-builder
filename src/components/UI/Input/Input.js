6d848daf12c8b94bd452b0a7d3d4f99dfbe9ff02
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-03 21:14:36 +0200

    added onChange method reference to component up

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -8,18 +8,23 @@ const input = (props) => {
         case ('input'):
             inputElement = <input
                 className={classes.InputElement}
-                {...props.elementConfig} value={props.value} />;
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
             break;
         case ('textarea'):
             inputElement = <textarea
                 className={classes.InputElement}
-                {...props.elementConfig} value={props.value} />
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />
             break;
         case ('select'):
             inputElement = (
                 <select
                     className={classes.InputElement}
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option
                             key={option.value}
