8eb7f08ae3a8733cba7ebf00d48eca063eb755ae
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-23 20:17:46 +0200

    added

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,2 +1,12 @@
 import * as actionTypes from './actions';
 
+const initState = {
+    ingredients: null,
+    totalPrice: 4,
+};
+
+const reducer = (state = initState, action) => {
+
+};
+
+export default reducer;
