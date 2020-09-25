93818d39f814686508c13bd9082a947e7f3a4679
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-25 18:06:58 +0200

    deleted unused

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,30 +5,6 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends React.Component {
-    state = {
-        ingredients: null,
-        totalPrice: 0
-    };
-
-    componentWillMount() {
-        const query = new URLSearchParams(this.props.location.search);
-        const ingredients = {};
-        let price = 0;
-
-        for (let param of query.entries()) {
-            if (param[0] === 'price') {
-                price = param[1];
-            } else {
-                ingredients[param[0]] = +param[1];
-            }
-
-        }
-
-        this.setState({
-            ingredients: ingredients,
-            totalPrice: price
-        });
-    }
 
     checkoutCancellHandler = () => {
         this.props.history.goBack();
