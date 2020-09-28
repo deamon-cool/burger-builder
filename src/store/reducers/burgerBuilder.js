50ae47fec33e60f1a749638cbd54a27fec6c5f3f
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-28 10:19:51 +0200

    set null because ingredients are fetching from server

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,12 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initState = {
-    ingredients: {
-        salad: 0,
-        bacon: 0,
-        cheese: 0,
-        meat: 0
-    },
+    ingredients: null,
     totalPrice: 4,
 };
 
