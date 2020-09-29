d1d8cd12d8c45a3527c8dc295836bb3afe722141
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-29 10:48:50 +0200

    added synchronous action creators

---
+++ src/store/actions/order.js
@@ -0,0 +1,16 @@
+import * as actionTypes from './actionTypes';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error: error
+    };
+};
