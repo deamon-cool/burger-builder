70c9a00320cb29928548222f4f939d172a415faa
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-29 14:25:25 +0200

    added action and new state variable to reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -2,11 +2,17 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initState = {
     orders: [],
-    loading: false
+    loading: false,
+    purchased: false
 };
 
 const reducer = (state = initState, action) => {
     switch (action.type) {
+        case actionTypes.PURCHASE_INIT:
+            return {
+                ...state,
+                purchased: false
+            };
         case actionTypes.PURCHASE_BURGER_START:
             return {
                 ...state,
@@ -21,6 +27,7 @@ const reducer = (state = initState, action) => {
             return {
                 ...state,
                 loading: false,
+                purchased: true,
                 orderds: state.orders.concat(newOrder)
             };
         case actionTypes.PURCHASE_BURGER_FAIL:
