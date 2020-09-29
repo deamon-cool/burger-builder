b717e0a8a8f3f3c581569df53818cbe27a3f7a53
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-29 11:48:05 +0200

    added order reducer switch action

---
+++ src/store/reducers/order.js
@@ -0,0 +1,31 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initState = {
+    orders: [],
+    loading: false
+};
+
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            };
+
+            return {
+                ...state,
+                loading: false,
+                orderds: state.orders.concat(newOrder)
+            };
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
