845d7047303e9f9c75c2e84f0179e3134fd3de7d
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-29 10:51:35 +0200

    added asynchronous action creator

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import config from '../../config-fetch';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
@@ -12,5 +13,24 @@ export const purchaseBurgerFail = (error) => {
     return {
         type: actionTypes.PURCHASE_BURGER_FAIL,
         error: error
+    };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        const init = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(orderData)
+        };
+
+        fetch(config.url + 'orders.json', init)
+            .then(res => {
+                this.setState({ loading: false });
+                this.props.history.push('/');
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            });
     };
 };
