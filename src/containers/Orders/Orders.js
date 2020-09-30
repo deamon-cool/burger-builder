4901cfa3e760880f8f2ef227aaadaa9799ce8feb
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-30 10:04:47 +0200

    added loading spinner

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import Order from '../../components/Order/Order';
 import actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner';
 
 class Orders extends React.Component {
     componentDidMount() {
@@ -10,20 +11,25 @@ class Orders extends React.Component {
     }
 
     render() {
+        let orders = <Spinner />;
+        if (!this.props.loading) {
+            orders = this.props.orders.map(order => (
+                <Order
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={+order.price} />
+            ));
+        }
+
         return (
             <div>
-                {this.props.orders.map(order => (
-                    <Order
-                        key={order.id}
-                        ingredients={order.ingredients}
-                        price={+order.price} />
-                ))}
+                {orders}
             </div>
         );
     }
 }
 
-const mapStateToProps = state=>{
+const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.loading
