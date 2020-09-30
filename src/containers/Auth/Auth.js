0b285e96478d8e1851557a0e2a3b067b0b5ed0ac
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-30 12:12:51 +0200

    imported

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends React.Component {
     state = {
