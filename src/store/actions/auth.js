8a190675eaabad0ea51c77c82b8375f5e09249b4
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-30 15:21:05 +0200

    added post request to authentication firebase server

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,5 +23,28 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+
+        const init = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(authData)
+        };
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4NOAuGRsfAEm27jdqQkNFEcWWxiG_EzM', init)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                dispatch(authSucces(data));
+            }).catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
+        // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
     };
 };
