9fffae48ab587627f642ee89bb04006e32c73b9d
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-30 10:01:04 +0200

    imported

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Order from '../../components/Order/Order';
+import actions from '../../store/actions/index';
 
 class Orders extends React.Component {
     state = {
