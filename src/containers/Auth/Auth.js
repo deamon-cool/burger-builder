fdc60445d1af1203b5b8b6695dfe3fa35959f304
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-30 11:43:32 +0200

    added check validity

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -38,6 +38,35 @@ class Auth extends React.Component {
         }
     };
 
+    checkValidity = (value, rules) => {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = (value.length >= rules.minLength) && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = (value.length <= rules.maxLength) && isValid;
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
+
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.controls) {
