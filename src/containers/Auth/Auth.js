2c2f33e948993b5baa0abae678a17f0716b6e8e3
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-30 11:19:42 +0200

    added basic structure

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Auth extends React.Component {
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail Address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false
+            },
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+
+                </form>
+            </div>
+        );
+    }
+}
