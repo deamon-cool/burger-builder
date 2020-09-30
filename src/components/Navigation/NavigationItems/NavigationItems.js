cde834c343115f58f01f1f4cde40e5b6f57bff52
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-30 11:33:24 +0200

    deleted comment, added navigation to auth component

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/'>Burger Builder</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
-        {/* <NavigationItem link='/'>Checkout</NavigationItem> */}
+        <NavigationItem link='/auth'>Authorization</NavigationItem>
     </ul>
 )
 
