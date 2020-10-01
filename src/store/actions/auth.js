682818aad91d5a6a32c6b624031e295794200797
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 11:14:09 +0200

    added synchronous action creator

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,6 +21,12 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
 export const checkAuthTimeout = (expirationTime) => {
     return dispatch => {
         setTimeout(()=>{
