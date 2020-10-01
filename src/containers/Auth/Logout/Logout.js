88812f975a9b08445bec5534a66570dea128408b
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 13:39:37 +0200

    corrected

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
-import actions from '../../../store/actions/index';
+import * as actions from '../../../store/actions/index';
 
 class Logout extends React.Component {
     componentDidMount() {
