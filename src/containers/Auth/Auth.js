806c1e031a9e017d9223407c929c7c68f683b107
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 09:26:51 +0200

    addjusted isSignup in auth component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -85,7 +85,7 @@ class Auth extends React.Component {
     submitHandler = (event) => {
         event.preventDefault();
         this.props.onAuth(this.state.controls.email.value,
-            this.state.controls.password.value);
+            this.state.controls.password.value, this.state.isSignup);
     }
 
     switchAuthModeHandler = () => {
@@ -137,7 +137,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     };
 };
 
