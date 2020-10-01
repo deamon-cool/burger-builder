2ba773bd71dc31c622d8a65eafd6c10b146adf97
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 10:34:40 +0200

    corrected

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -6,10 +6,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -50,7 +51,7 @@ export const auth = (email, password, isSignup) => {
                 if (data.error) {
                     dispatch(authFail(data.error));
                 } else {
-                    dispatch(authSuccess(data));
+                    dispatch(authSuccess(data.idToken, data.localId));
                 }
             }).catch(err => {
                 console.log(err);
