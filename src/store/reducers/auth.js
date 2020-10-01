0198bb1a2289c894caa00ae564d184d2023aa5aa
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 11:16:27 +0200

    added new function in reducer auth that will handle logout user

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -31,6 +31,13 @@ const authFail = (state, action) => {
     });
 };
 
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null
+    });
+}
+
 const reducer = (state = initState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START:
