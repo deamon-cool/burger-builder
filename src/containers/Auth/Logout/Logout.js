5d124c94f9b1976bd9a990756cd5afe7db72abea
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 13:16:15 +0200

    added structure with react and redux for logout container

---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Logout extends React.Component {
+    render() {
+        return (
+
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Logout);
