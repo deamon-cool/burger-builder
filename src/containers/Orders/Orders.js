25c581263071d559f9d865225134b09fef5ac75c
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 11:49:37 +0200

    added argument token

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -38,7 +38,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
     };
 }
 
