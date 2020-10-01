02fdd2bf6bdbe50ec17cb142dbac705e658762f7
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-01 13:22:43 +0200

    finished Logout subcontainer

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import actions from '../../../store/actions/index';
 
 class Logout extends React.Component {
-    render() {
-        return (
+    componentDidMount() {
+        this.props.onLogout();
+    }
 
-        );
+    render() {
+        return <Redirect to='/' />;
     }
 }
 
