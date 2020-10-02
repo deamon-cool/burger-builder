8724153a5db31fcb0ffc10febb5d2cf517aa16b5
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-02 11:37:13 +0200

    added

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends React.Component {
           <Route path='/orders' component={Orders} />
           <Route path='/logout' component={Logout} />
           <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/' />
         </Switch>
       );
     }
