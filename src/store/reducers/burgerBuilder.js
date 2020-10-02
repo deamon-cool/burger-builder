6c2dcba99f2ece6c144017f7abb5a310450f324a
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-02 08:36:32 +0200

    added

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -46,7 +46,8 @@ const reducer = (state = initState, action) => {
                     meat: action.ingredients.meat,
                 },
                 totalPrice: 4,
-                error: false
+                error: false,
+                building: false
             };
         case actionTypes.FETCH_INGREDIENTS_FAILED:
             return {
