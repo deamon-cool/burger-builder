0f33e3a8e9daf2b7814c1312d89a3631ae732079
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-02 10:27:05 +0200

    updated with time

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -98,8 +98,12 @@ export const authCheckState = () => {
             dispatch(logout());
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
+
             if (expirationDate > new Date()) {
-                dispatch(authSuccess(token,));
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
             } else {
                 dispatch(logout());
             }
