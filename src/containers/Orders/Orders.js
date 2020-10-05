3299ba568692b0c9e11b8f73def8ad77516aac61
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-05 13:28:57 +0200

    added specific dependcies to useEffect

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -6,9 +6,11 @@ import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 function Orders(props) {
+    const { onFetchOrders } = props
+
     useEffect(() => {
         props.onFetchOrders(props.token, props.userId);
-    }, []);
+    }, [onFetchOrders]);
 
     let orders = <Spinner />;
     if (!props.loading) {
