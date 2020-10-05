eaca4d0536c8a77dec20f4a8eaaf237353a1821c
daemon-cool <damian.damian.tofik@gmail.com>
2020-10-05 11:38:46 +0200

    changed class onto hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -10,41 +10,38 @@ import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
-class App extends React.Component {
+function App(props) {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []);
 
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
+  let routes = (
+    <Switch>
+      <Route path='/auth' component={Auth} />
+      <Route path='/' exact component={BurgerBuilder} />
+      <Redirect to='/' />
+    </Switch>
+  );
 
-  render() {
-    let routes = (
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path='/auth' component={Auth} />
+        <Route path='/checkout' component={Checkout} />
+        <Route path='/orders' component={Orders} />
+        <Route path='/logout' component={Logout} />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/logout' component={Logout} />
-          <Route path='/' exact component={BurgerBuilder} />
-          <Redirect to='/' />
-        </Switch>
-      );
-    }
-
-    return (
-      <div>
-        <Lauout>
-          {routes}
-        </Lauout>
-      </div>
-    );
   }
+
+  return (
+    <div>
+      <Lauout>
+        {routes}
+      </Lauout>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
